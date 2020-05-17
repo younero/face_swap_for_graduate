d8dd7364f6bc9bd3b624b0ddc243c56fe4335ccf
younero <45897811+younero@users.noreply.github.com>
2020-05-17 22:22:37 +0800

    update

--- server.py
+++ server.py
@@ -15,9 +15,13 @@ def swap_page():
     resp.headers["Content-Type"] = "image/jpeg"
     return resp
 
+@app.route('/notice')
+def notice():
+    return render_template('notice.html')
+
 @app.route('/')
 def index():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80)
+    app.run(host='0.0.0.0', port=80, debug=True)
